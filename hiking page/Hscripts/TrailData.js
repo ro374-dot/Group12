eada5b6ac42b70298a14c480997c8195279c349e
ro374-dot <ro374@nau.edu>
2025-11-17 17:00:21 -0700

    Update TrailData.js

--- hiking page/Hscripts/TrailData.js
+++ hiking page/Hscripts/TrailData.js
@@ -12,7 +12,7 @@ function createTrail(name, rating, maxRating, address, description, imageUrl) {
 
 // Function to fetch and display trails
 function loadTrails() {
-  fetch('hiking page/api/trails')
+  fetch('api/trails')
     .then(response =&gt; response.json())
     .then(data =&gt; {
       const trails = data.map(trail =&gt; createTrail(
