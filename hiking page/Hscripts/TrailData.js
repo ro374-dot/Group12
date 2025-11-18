ed80296e7f506a2da38c64379db589fb07025a4c
ro374-dot <ro374@nau.edu>
2025-11-17 20:48:27 -0700

    Update TrailData.js

--- hiking page/Hscripts/TrailData.js
+++ hiking page/Hscripts/TrailData.js
@@ -16,12 +16,12 @@ function loadTrails() {
     .then(response => response.json())
     .then(data => {
       const trails = data.map(trail => createTrail(
-        trail.trail_name,
+        trail.name,
         trail.rating,
         trail.max_rating,
         trail.address,
         trail.description,
-        trail.image_url
+        trail.photo
       ));
 
       const container = document.getElementById('trails-container');
@@ -31,8 +31,8 @@ function loadTrails() {
         trailDiv.className = 'trail';
 
         trailDiv.innerHTML = `
-          <h2>${trail.trail_name}</h2>
-          <img src="${trail.image_url}" alt="${trail.trail_name}">
+          <h2>${trail.name}</h2>
+          <img src="${trail.photo}" alt="${trail.name}">
           <p><strong>Rating:</strong> ${trail.rating} / ${trail.max_rating}</p>
           <p><strong>Address:</strong> ${trail.address}</p>
           <p>${trail.description}</p>
